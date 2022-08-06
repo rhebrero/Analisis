b2952aea819b824cb082fa3e4ad56f17299d90be
Alberto Escalante <a.escalante.del.valle@cern.ch>
2022-08-06 21:51:38 +0200

    added more flexibility, expanded to >1 selection

--- DDM/SimpleTTree/reader.py
+++ DDM/SimpleTTree/reader.py
@@ -1,15 +1,14 @@
 import ROOT
-from ROOT import gPad
-
+#import pdb to DEBUG
 def getTree(myFile, treeDir, treeName, label):
-    
+
     f = ROOT.TFile.Open(myFile)
     tdir = f.Get(treeDir)
     t = tdir.Get(treeName)
     return f, t, label
 
 def createPlots(variable, trees, labels, outputFolder):
-    
+
     #variable configuration.
     variableName = variable["var_name"]
     selection = variable["selection"]
@@ -21,38 +20,41 @@ def createPlots(variable, trees, labels, outputFolder):
     c1=ROOT.TCanvas("c1")
 
     hist_dic = {}
-    hist_index = 0
-    colors = [ROOT.kRed, ROOT.kBlue, ROOT.kGreen]
+    colors = [ROOT.kRed, ROOT.kBlue, ROOT.kGreen, ROOT.kOrange]
     legend = ROOT.TLegend (0.15 ,0.7 ,0.35 ,0.85)
-    for ktree in trees:
-        hist_dic["hist_"+str(hist_index)] = ROOT.TH1D(labels[hist_index], selection, nbins, inibin, endbin)        
-        plotlabel = "hist SAME"
-        if hist_index == 1 : plotlabel = "SAMES"
-        ktree.Draw(variableName+" >> "+labels[hist_index], selection, "goff")
-        if norm == True and hist_dic["hist_"+str(hist_index)].GetEntries()>0:
-            hist_dic["hist_"+str(hist_index)].Scale(1/hist_dic["hist_"+str(hist_index)].GetEntries())
-        hist_dic["hist_"+str(hist_index)].Draw(plotlabel)
-        hist_dic["hist_"+str(hist_index)].SetLineColor(colors[hist_index])
-        hist_dic["hist_"+str(hist_index)].GetXaxis().SetTitle(variableName)
+    for index, ktree in enumerate(trees):
+        if index > 3:
+            print("reader.py only allows up to 4 histograms")
+            break
+        hist_dic["hist_"+str(index)] = ROOT.TH1D(labels[index], selection[index], nbins, inibin, endbin)
+        plotlabel = "hist"
+        if index > 0 : plotlabel = "SAMES"
+        ktree.Draw(variableName+" >> "+labels[index], selection[index], "goff")
+        if norm == True and hist_dic["hist_"+str(index)].GetEntries()>0:
+            hist_dic["hist_"+str(index)].Scale(1/hist_dic["hist_"+str(index)].GetEntries())
+        hist_dic["hist_"+str(index)].Draw(plotlabel)
+        hist_dic["hist_"+str(index)].SetLineColor(colors[index])
+        hist_dic["hist_"+str(index)].GetXaxis().SetTitle(variableName)
         xlabel = "Events"
         if norm == True:
             xlabel = "Events. norm."
-        hist_dic["hist_"+str(hist_index)].GetYaxis().SetTitle(xlabel)
-        
-        gPad.Update()
-        stat = hist_dic["hist_"+str(hist_index)].FindObject("stats")
-        stat.SetTextColor(colors[hist_index])        
-        hist_dic["hist_"+str(hist_index)].FindObject("stats").SetX1NDC(0.80-0.2*hist_index)
-        hist_dic["hist_"+str(hist_index)].FindObject("stats").SetX2NDC(0.99-0.2*hist_index)
+        hist_dic["hist_"+str(index)].GetYaxis().SetTitle(xlabel)
 
-        hist_index = hist_index +1
+        ROOT.gPad.Update()
+        stat = hist_dic["hist_"+str(index)].FindObject("stats")
+        stat.SetTextColor(colors[index])
+        hist_dic["hist_"+str(index)].FindObject("stats").SetX1NDC(0.80-0.2*index)
+        hist_dic["hist_"+str(index)].FindObject("stats").SetX2NDC(0.99-0.2*index)
 
-                
+
     #output files.
     formats = [".png", ".pdf", ".tex", ".C"]
     suffix = ""
     if norm == True:
         suffix = suffix + "_norm"
     for kformat in formats:
-        c1.SaveAs(outputFolder+variableName+suffix+kformat)
-
+        filename = outputFolder+variableName+suffix
+        filename = filename.replace("()", "") #cleanup the filename
+        filename = filename.replace(".", "_") #cleantup the filename
+        filename = filename+kformat
+        c1.SaveAs(filename)
