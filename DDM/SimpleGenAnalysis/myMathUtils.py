0b6c75907436c42fb88a07de9e2ae42c1ace7b76
alberto escalante <a.escalante.del.valle@cern.ch>
2022-11-20 11:55:48 +0100

    added invariant mass calculation

--- DDM/SimpleGenAnalysis/myMathUtils.py
+++ DDM/SimpleGenAnalysis/myMathUtils.py
@@ -1,14 +1,25 @@
 from math import *
 from RecoUtils import *
-from myMathUtils import*
 
 def deltaPhi(a,b):
+    '''
+    computes delta phi for two given gen particles
+    '''
     dphi = abs(a.phi()-b.phi())
     if dphi > pi: dphi = 2*pi-dphi
     return dphi
 
 def deltaR(a,b):
+    '''
+    computes dR for two given gen particles
+    '''
     dphi = deltaPhi(a,b)
     return hypot(a.eta()-b.eta(),dphi)
 
-                    
+def mass(a, b):
+    '''
+    computes the invariant mass for two given gen particles
+    '''
+    mass2 = 2*a.pt()*b.pt()*( cosh(a.eta()-b.eta()) - cos(deltaPhi(a,b)))
+    mass = sqrt(mass2)
+    return mass
